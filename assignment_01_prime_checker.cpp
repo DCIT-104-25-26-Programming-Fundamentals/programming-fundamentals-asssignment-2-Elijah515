2cac1c59e12a6b67baa27125efcf5c613485e50f
Elijah <asamoahadofoelijah515gmail.com>
2026-07-30 20:12:10 +0000

    Implement C++ prime checker using is_prime function

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,27 @@
 #include <iostream>
 using namespace std;
 
+bool is_prime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+if (is_prime(n) == true) {
+        cout << n << " is a prime number." << endl;
+    } else {
+        cout << n << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
